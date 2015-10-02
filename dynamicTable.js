9698e74869064b92081c002bc33e0ff995eee7f8
Daniel Hornik <daniel.1302@gmail.com>
2015-10-02 14:58:15 +0200

    Project init

---
+++ dynamicTable.js
@@ -0,0 +1,26 @@
+function DynamicTable() {
+    this.config = {
+        columns: {},
+        
+    };
+    
+    this.init = function() {
+        this.bindEvents();
+    }
+    
+    this.clear = function() {
+        
+    }
+    
+    this.draw = function() {
+        
+    }
+    
+    this.bindEvents = function() {
+        
+    }
+    
+    this.setData = function(data) {
+        
+    }
+}
